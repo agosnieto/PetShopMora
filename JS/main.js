7e96b3e76a904130fbfb01ea423203000401531f
Jael Agostina Nieto Camino <agosnica42@gmail.com>
2021-11-23 12:24:35 -0300

    Primera clase de JS

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+// Nombre del usuario y páis de origen
+let ingreseUsuario = prompt("Ingrese su nombre por favor");
+let pais = prompt("Ingres su país de origen")
+
+// Edad del usuario
+let edad = prompt("Ingrese su edad por favor:");
+
+alert(" Bienvenido/a " + " " + ingreseUsuario + " del país" + " " + pais + " de " + edad + " años de edad ");
+
+//Edad de los padres del usuario
+
+let madreEdad = parseInt(prompt("Ingrese edad de su madre"));
+let padreEdad =  parseInt( prompt("Ingrese edad de su padre"));
+let resultado = madreEdad + padreEdad;
+
+alert(" La edad de sus padres suma" + " " + resultado + " años");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
