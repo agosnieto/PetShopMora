b39c1ffe9c495652cc175a08bac403b0778fd8ba
Jael Agostina Nieto Camino <agosnica42@gmail.com>
2021-11-28 22:23:35 -0300

    segundo desafío js

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,48 @@
 // Nombre del usuario y páis de origen
 let ingreseUsuario = prompt("Ingrese su nombre por favor");
 let pais = prompt("Ingres su país de origen")
+alert(" Bienvenido/a " + " " + ingreseUsuario + " del país" + " " + pais);
 
-// Edad del usuario
-let edad = prompt("Ingrese su edad por favor:");
+// Declaración de variables
 
-alert(" Bienvenido/a " + " " + ingreseUsuario + " del país" + " " + pais + " de " + edad + " años de edad ");
+let balanceado = "balanceado";
+let precio1 = 1200;
+let stock1 = 50;
 
-//Edad de los padres del usuario
+let accesorios = "accesorios"
+let precio2 = 500;
+let stock2 = 20;
 
-let madreEdad = parseInt(prompt("Ingrese edad de su madre"));
-let padreEdad =  parseInt( prompt("Ingrese edad de su padre"));
-let resultado = madreEdad + padreEdad;
+// Edad del usuario y uso de condcionales
 
-alert(" La edad de sus padres suma" + " " + resultado + " años");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let edad =prompt("Ingrese su edad por favor:");
+if (edad >= 18){
+    let respuesta = prompt("¿Desea adquirir algún producto?")
+    if (respuesta == "si"){
+        let compra = prompt("Ingrese producto a comprar, 'balanceado o accesorios'");
+        if (compra == balanceado || compra == accesorios){
+        let cantidad = prompt("Ingrese cantidad del producto que desea");
+            if (compra == balanceado){
+                stock1 -= cantidad
+                alert("El precio a pagar es $"+ precio1 * cantidad);
+            }
+            else{
+                stock2 -= cantidad
+                alert("El precio a pagar es $"+ precio2 * cantidad);
+            }
+        }
+        else{
+            alert("No se encontró el producto solicitado")
+        }
+    }
+}
+else{
+    alert("Recuerde que al ser menor de edad no podrá realizar compras sin supervisión de un adulto")
+    let respuesta2 = prompt ("¿Desea continuar?")
+    if (respuesta2 == "si"){
+        alert("Continuemos!!")
+    }
+    else{
+        alert("¡Muchas gracias por visitar nuestra página!")
+    }
+}
